81a54f37e44d143b4e63750fafd2500595c0d079
Абдулкадир Гаджиламаммаев <abdylkadir2004@gmail.com>
2023-01-11 17:23:27 +0300

    second page

--- src/components/AboutRoom/AboutRoom.jsx
+++ src/components/AboutRoom/AboutRoom.jsx
@@ -1,4 +1,5 @@
 import FeedBack from "@components/FeedBack"
+import { Button } from 'antd';
 import img1 from "@components/AboutRoom/image/room1.jpg"
 import img2 from "@components/AboutRoom/image/room2.jpg"
 import img3 from "@components/AboutRoom/image/room3.jpg"
@@ -9,19 +10,24 @@ const AboutRoom = () => {
     return (
         <section className="room">
             <div className="container">
-                <div className="room__row">
-                    <div className="room__column">
+                <div className="room__column">
+                    <div className="room__row">
                         <div className="room__content">
-                            <h1>Портлендский крошечный дом (Альберта искусств)</h1>
+                            <h1 className="room__title">Портлендский крошечный дом (Альберта искусств)</h1>
                             <span>(230 Ratings)</span> <FeedBack />
                         </div>
-                        <div className="room__img"><img src={img1} alt="room-image1" /></div>
+                        <div className="room__btn">
+                            <Button type="primary" ghost size='large'>
+                                Показать больше фотографий
+                            </Button>
+                        </div>
                     </div>
-                    <div className="room__column">
+                    {/* <div className="room__row">
+                        <div className="room__img-big"><img src={img1} alt="room-image1" /></div>
                         <div className="room__images">
                             <div className="room__img"><img src={img2} alt="room-image2" /></div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </section>
